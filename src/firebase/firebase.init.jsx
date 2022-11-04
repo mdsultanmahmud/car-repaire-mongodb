7add097ac580190f463cfa16569e184cc1307311
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-11-04 22:47:30 +0600

    creating car repaire website with mongodb

---
+++ src/firebase/firebase.init.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxDWj9tP_9_u7GruDlT-CehHXnWouiUSQ",
+  authDomain: "car-repaire-mongodb-e0738.firebaseapp.com",
+  projectId: "car-repaire-mongodb-e0738",
+  storageBucket: "car-repaire-mongodb-e0738.appspot.com",
+  messagingSenderId: "875904061696",
+  appId: "1:875904061696:web:b59f2eec6342eaaea3e3e5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app 
