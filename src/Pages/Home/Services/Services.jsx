8598c6bb07e01991ca92d6cc06c61a36733837e5
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-11-11 22:13:28 +0600

    data sorting implement

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,31 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ServicesItem from './ServicesItem';
 
 const Services = () => {
     const [serciesItem, setServicesItem] = useState([])
-    useEffect(() =>{
-        fetch('https://car-with-mongodb-server.vercel.app/services')
-        .then(res => res.json())
-        .then(data => setServicesItem(data))
-    } ,[])
+    const [isAsc, setIsAscc] = useState(true)
+    const [searchString, setSearchString] = useState('')
+    const searchRef = useRef()
+    useEffect(() => {
+        fetch(`http://localhost:5000/services?search=${searchString}&order=${isAsc ? 'asc' : 'des'}`)
+            .then(res => res.json())
+            .then(data => setServicesItem(data))
+    }, [isAsc, searchString])
+
+    const serachHandling = () =>{
+        setSearchString(searchRef.current.value)
+    }
+    console.log(searchString)
     return (
         <div>
             <div>
                 <p className='text-orange-700 font-bold text-center'>Services</p>
                 <h3 className='text-center font-bold text-3xl py-2'>Our Services Area</h3>
                 <p className='text-center py-3 '>the majority have suffered alteration in some form, <br /> but injected humous, or randomised words which don't lock even slightly believable.</p>
+                <div className='my-5 text-center'>
+                    <input ref={searchRef} className='input bg-white' type="text" />
+                    <button onClick={serachHandling} className='btn btn-primary ml-2'>Search</button>
+                </div>
+                <button onClick={() => setIsAscc(!isAsc)} className='my-4 btn btn-outline btn-secondary mx-auto block px-12'>{isAsc ? 'des' : 'asc'}</button>
             </div>
             <div className='grid gap-5 p-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
                 {
-                    serciesItem.map(item => <ServicesItem 
+                    serciesItem.map(item => <ServicesItem
                         key={item._id}
-                        srItem = {item}
+                        srItem={item}
                     ></ServicesItem>)
                 }
-                
+
             </div>
             <button className="btn btn-outline btn-secondary mx-auto my-8 block">More Services</button>
-        </div> 
+        </div>
     );
 };
 
